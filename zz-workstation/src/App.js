b18fc47015377e0145f769cc49598e43979ad495
Jason <jason.jafari.m@gmail.com>
2020-07-11 13:28:02 -0400

    make change for child compoenents

--- zz-workstation/src/App.js
+++ zz-workstation/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import "./App.css";
+import Person from './components/Person';
 
 class App extends Component {
     constructor() {
@@ -11,6 +12,8 @@ class App extends Component {
         return (
             <div className="App">
                 <h1>Hello World!</h1>
+                <hr />
+                <Person />
             </div>
         );
     }
