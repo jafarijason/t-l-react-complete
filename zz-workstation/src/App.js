0de9cfbaa756497637e49d9d9a0973b64a243fc6
Jason <jason.jafari.m@gmail.com>
2020-07-11 13:21:50 -0400

    Hello World

---
+++ zz-workstation/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+import "./App.css";
+
+class App extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello World!</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
