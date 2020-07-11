243065bec466ad0fde79394c658dbcd62ca4416f
Jason <jason.jafari.m@gmail.com>
2020-07-11 13:48:05 -0400

    State handele with class

--- zz-workstation/src/App.js
+++ zz-workstation/src/App.js
@@ -4,20 +4,36 @@ import "./App.css";
 import Person from './components/Person';
 
 class App extends Component {
+
     constructor() {
         super();
+
+        this.state = {
+            persons: [
+                { firsName: "Jason", lastNmae: "Jafari", age: 33 },
+                { firsName: "Roy", lastNmae: "Marandi", age: 30 },
+                { firsName: "Sara", lastNmae: "RekabTalaei", age: 31 },
+            ]
+        }
     }
 
     render() {
+        const { persons } = this.state
         return (
             <div className="App">
                 <h1>Hello World!</h1>
                 <hr />
-                <Person 
-                firstName="Jason"
-                lastName="Jafari"
-                Age="25"
-                 />
+
+                {
+                    persons.map(person => (
+                        <Person
+                            firstName={person.firsName}
+                            lastName={person.lastNmae}
+                            Age={person.age}
+                        />
+                    ))
+                }
+
             </div>
         );
     }
