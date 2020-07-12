676feaa29d400051c07a99451ca615f99a59dc36
Jason <jason.jafari.m@gmail.com>
2020-07-12 03:03:20 -0400

    add css to person

--- zz-workstation/src/components/Person/Person.jsx
+++ zz-workstation/src/components/Person/Person.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Person.css'
 
 const Person = ({ fullName, age, personDelete,personChange }) => {
   return (
-    <div style={{ cursor: "pointer" }}>
+    <div style={{ cursor: "pointer" }} className="person">
       <p  >{`${fullName} - Age: ${age}`}</p>
       <input type="text" placeholder={fullName} onChange={personChange}/>
       <button onClick={personDelete} >delete</button>
