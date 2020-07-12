b285d0b9177aa50dadfc2595fdb3a0cc20e060e9
Jason <jason.jafari.m@gmail.com>
2020-07-12 11:28:20 -0400

    use from use context

--- zz-workstation/src/components/common/Header.jsx
+++ zz-workstation/src/components/common/Header.jsx
@@ -1,34 +1,37 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import { Alert, Badge } from 'react-bootstrap'
 import SimpleContext from '../../context/SimpleContext';
 
 
-const Header = ({ PersonsLength  }) => {
+const Header = () => {
+
+  const context = useContext(SimpleContext)
+
   let badgeStyle = "";
 
-  if (PersonsLength >= 3) badgeStyle = "success";
-  if (PersonsLength <= 2) badgeStyle = "warning";
-  if (PersonsLength <= 1) badgeStyle = "danger";
+  if (context.state.persons.length >= 3) badgeStyle = "success";
+  if (context.state.persons.length <= 2) badgeStyle = "warning";
+  if (context.state.persons.length <= 1) badgeStyle = "danger";
   return (
-    <SimpleContext.Consumer >
-      {
-        context => (
-          <div>
-            <Alert variant="info">
-              <h2>{context.state.appTitle}</h2>
-            </Alert>
-            <Alert variant="light">
-              تعداد اشخاص{" "}
-              <Badge pill variant={badgeStyle}>
-                {context.state.persons.length}
-              </Badge>{" "}
+    // <SimpleContext.Consumer >
+    //   {
+    //     context => (
+    <div>
+      <Alert variant="info">
+        <h2>{context.state.appTitle}</h2>
+      </Alert>
+      <Alert variant="light">
+        تعداد اشخاص{" "}
+        <Badge pill variant={badgeStyle}>
+          {context.state.persons.length}
+        </Badge>{" "}
                     نفر می باشد
                </Alert>
-          </div>
-        )
-      }
-    </SimpleContext.Consumer>
+    </div>
+    //     )
+    //   }
+    // </SimpleContext.Consumer>
   );
 }
 
