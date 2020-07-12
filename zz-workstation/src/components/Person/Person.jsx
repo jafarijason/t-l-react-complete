8d6676b2ed26445bb2b925968452bc800ad56177
Jason <jason.jafari.m@gmail.com>
2020-07-12 03:46:20 -0400

    Persons manager css styling

--- zz-workstation/src/components/Person/Person.jsx
+++ zz-workstation/src/components/Person/Person.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
-import './Person.css'
+import React from "react";
 
-const Person = ({ fullName, age, personDelete,personChange }) => {
-  return (
-    <div style={{ cursor: "pointer" }} className="person">
-      <p  >{`${fullName} - Age: ${age}`}</p>
-      <input type="text" placeholder={fullName} onChange={personChange}/>
-      <button onClick={personDelete} >delete</button>
-    </div>
-  );
-}
+const Person = ({ fullname, deleted, changed }) => {
+    return (
+        <div className="card text-white bg-info mb-3 mt-3 w-25 mx-auto">
+            <div className="card-body text-center">
+                <p className="d-block">{`${fullname}`}</p>
+                <div className="input-group justify-content-center">
+                    <input
+                        type="text"
+                        className="form-control w-50"
+                        placeholder={fullname}
+                        onChange={changed}
+                    />
+                    <div className="input-group-prepend">
+                        <button
+                            className="btn btn-sm btn-danger fa fa-trash"
+                            onClick={deleted}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default Person;
--- zz-workstation/src/components/Person/Persons.jsx
+++ zz-workstation/src/components/Person/Persons.jsx
@@ -1,24 +1,19 @@
-import React from 'react'
-import Person from './Person'
+import React from "react";
+import Person from "./Person";
 
 const Persons = ({ persons, personDelete, personChange }) => {
-  // console.log(persons)
-  return (
-    <div>
-      {
-        persons.map(person => (
-          <Person
-            fullName={person.fullName}
-
-            age={person.age}
-            key={person.id}
-            personDelete={() => personDelete(person.id)}
-            personChange={(event) => personChange(event, person.id)}
-          />
-        ))
-      }
-    </div>
-  );
-}
+    return (
+        <div>
+            {persons.map(person => (
+                <Person
+                    key={person.id}
+                    fullname={person.fullname}
+                    deleted={() => personDelete(person.id)}
+                    changed={event => personChange(event, person.id)}
+                />
+            ))}
+        </div>
+    );
+};
 
 export default Persons;
